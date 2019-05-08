acc6524758eb8a9ffd82f147270f71987945709e
Shawn-Nevermore <shawnxu2016sg@gmail.com>
2019-05-08 15:14:47 +0800

    加注释，改了菜单不对齐的bug，menuItem的空隙bug仍然存在，疑似图片尺寸问题:

--- apple-like-gallery/main.js
+++ apple-like-gallery/main.js
@@ -1,59 +1,49 @@
 $(document).ready(function () {
-    /* This code is executed after the DOM has been completely loaded */
 
-    var totWidth = 0;
-    var positions = new Array();
+    var totWidth = 0
+    var positions = new Array()
 
     $('#slides .slide').each(function (i) {
 
         /* Traverse through all the slides and store their accumulative widths in totWidth */
+        // 遍历所有slide，然后将累加宽度添加至totWidth
 
-        positions[i] = totWidth;
-        totWidth += $(this).width();
+        positions[i] = totWidth
+        totWidth += $(this).width()
 
         /* The positions array contains each slide's commulutative offset from the left part of the container */
+        // 该位置数组包含每个slide的累积offset，即从父元素最左边开始的距离
 
         if (!$(this).width()) {
-            alert("Please, fill in width & height for all your images!");
-            return false;
+            alert("Please, fill in width & height for all your images!")
+            return false
         }
 
-    });
+    })
 
-    $('#slides').width(totWidth);
+    $('#slides').width(totWidth)
 
-    /* Change the cotnainer div's width to the exact width of all the slides combined */
+    /* Change the container div's width to the exact width of all the slides combined */
+    // 改变容器div的宽度为所有slide的准确宽度值总和
 
     $('#menu ul li a').click(function (e, keepScroll) {
 
-        /* On a thumbnail click */
+        $('li.menuItem').removeClass('act').addClass('inact')
+        $(this).parent().addClass('act')
 
-        $('li.menuItem').removeClass('act').addClass('inact');
-        $(this).parent().addClass('act');
+        var pos = $(this).parent().prevAll('.menuItem').length
 
-        var pos = $(this).parent().prevAll('.menuItem').length;
+        $('#slides').stop().animate({ marginLeft: -positions[pos] + 'px' }, 450)
 
-        $('#slides').stop().animate({ marginLeft: -positions[pos] + 'px' }, 450);
-        /* Start the sliding animation */
+        // 阻止Link默认跳转
+        e.preventDefault()
 
-        e.preventDefault();
-        /* Prevent the default action of the link */
+        // 点击停止自动轮播
+        if (!keepScroll) clearInterval(itvl)
+    })
 
+    $('#menu ul li.menuItem:first').addClass('act').siblings().addClass('inact')
 
-        // Stopping the auto-advance if an icon has been clicked:
-        if (!keepScroll) clearInterval(itvl);
-    });
-
-    $('#menu ul li.menuItem:first').addClass('act').siblings().addClass('inact');
-    /* On page load, mark the first thumbnail as active */
-
-
-
-	/*****
-	 *
-	 *	Enabling auto-advance.
-	 *
-	 ****/
 
     var current = 1;
     function autoAdvance() {
@@ -65,9 +55,8 @@ $(document).ready(function () {
 
     // The number of seconds that the slider will auto-advance in:
 
-    var changeEvery = 10;
+    var changeEvery = 3;
 
     var itvl = setInterval(function () { autoAdvance() }, changeEvery * 1000);
 
-    /* End of customizations */
 })
